17706ae1d420f0ec209003c07738ddc0af8e0f47
Amadi Jerry <amadijerry310@gmail.com>
2024-05-19 17:23:28 +0100

    add users struct

--- models/users/users.go
+++ models/users/users.go
@@ -1,1 +1,8 @@
 package users
+
+type User struct {
+	Username string `json:"username" gorm:"unique"`
+	Email    string `json:"email" gorm:"unique"`
+	Password string `json: "password"`
+	Gender   string `json:"gender"`
+}
