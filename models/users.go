119f7ec98f86dccd8e7d5db09250a6cd6fc9b85c
Amadi Jerry <amadijerry310@gmail.com>
2024-06-23 18:06:29 +0100

    refactor user constraints

--- models/users.go
+++ models/users.go
@@ -11,16 +11,16 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID  `json:"id" gorm:"unique;type:uuid"`
+	ID        uuid.UUID  `json:"id" gorm:"type:uuid"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"-"`
 	DeletedAt *time.Time `json:"-"`
-	Username  string     `json:"username" gorm:"unique"`
-	Email     string     `json:"email" gorm:"primary_key"`
+	Username  string     `json:"username"`
+	Email     string     `json:"email" gorm:"unique"`
 	Password  string     `json:"password"`
 	Gender    string     `json:"gender"`
 	Picture   string     `json:"picture"`
-	AuthType  string     `json:"auth_type" gorm:"primary_key"`
+	AuthType  string     `json:"auth_type" gorm:"unique"`
 }
 
 const (
