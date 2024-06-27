d6aa4268aac4977a37017abbe7b60e2ff369ea97
Amadi Jerry <amadijerry310@gmail.com>
2024-06-27 09:50:21 +0100

    fix cors path issue

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,10 +1,19 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+	"github.com/gin-gonic/gin"
+)
 
 func CORSMiddleware() gin.HandlerFunc {
+	var path string
+	if os.Getenv("ENVIRONMENT") == "development"{
+		path = "http://localhost:3000"
+	}else{
+		path = "https://we-mingle.vercel.app"
+	}
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", path)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
